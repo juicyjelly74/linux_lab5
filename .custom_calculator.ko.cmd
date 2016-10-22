cd87fd767f1c7480acf4c66de75462568ad6bcf5
Jula <juicy.jelly74@gmail.com>
2016-10-22 16:32:48 -0700

    Added files

---
+++ .custom_calculator.ko.cmd
@@ -0,0 +1,1 @@
+cmd_/home/julia/Desktop/PVMS_5/linux_lab5/custom_calculator.ko := ld -r -m elf_x86_64 -T ./scripts/module-common.lds --build-id  -o /home/julia/Desktop/PVMS_5/linux_lab5/custom_calculator.ko /home/julia/Desktop/PVMS_5/linux_lab5/custom_calculator.o /home/julia/Desktop/PVMS_5/linux_lab5/custom_calculator.mod.o
---
+++ .tmp_versions/custom_calculator.mod
@@ -0,0 +1,2 @@
+/home/julia/Desktop/PVMS_5/linux_lab5/custom_calculator.ko
+/home/julia/Desktop/PVMS_5/linux_lab5/custom_calculator.o
